916912186819ad68ba56eb568a9f0043932d0eae
starfishda@naver.com <starfishda@naver.com>
2019-10-11 12:01:42 +0900

    .

--- [CODE] Assignment3/CLASS/object/Fruit.js
+++ [CODE] Assignment3/CLASS/object/Fruit.js
@@ -1,29 +1,33 @@
-class Bush extends DrawingObject {
+class Fruit extends DrawingObject {
     constructor(position) {
         super(position, vec2(1, 1));
-        this.z = -5;
+        this.z = -4;
     }
     static GetVertex(vertices) {
+		vertices.push(vec2(500, 500));
+        vertices.push(vec2(600, 400));
         // The center of the hexagon.
         vertices.push(vec2(500, 500));
 
         // From circle, move the angle 1 degrees to get the x, y. And add them to the array of vertexs.
         for (var i = 0; i <= 360; i++) {
             var agree = i;
-            var x = Math.cos(agree * Math.PI / 60.0) * 75 + 500;
+            var x = Math.cos(agree * Math.PI / 60.0) * 50 + 500;
             var y = Math.sin(agree * Math.PI / 60.0) * 50 + 500;
             vertices.push(vec2(x, y));
         }
         this.temp_vertices = vertices;
     }
     static GetColor(colors) {
-        for (var i = 0; i < 362; i++) {
+		colors.push(vec4(240, 100, 100, 255)); // center
+        for (var i = 0; i < 364; i++) {
             var a = this.temp_vertices[i][1] / 500;
-            colors.push(vec4(40 * a, 180 * a, 40 * a, 255)); // center
+            colors.push(vec4(240 * a, 40 * a, 40 * a, 255)); // center
         }
     }
     static GetDraw(drawlist) {
-        drawlist.push([gl.TRIANGLE_FAN, 0, 362])
+		drawlist.push([gl.LINES, 0, 2])
+        drawlist.push([gl.TRIANGLE_FAN, 3, 363])
     }
     
 	// 1/60초마다 불리는 함수
